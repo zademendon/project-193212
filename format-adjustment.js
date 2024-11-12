2a8c0bef39ea29e77eccc61e45b8775311ae7db1
zademendon <104177830+zademendon@users.noreply.github.com>
2024-11-12 18:29:00 -0300

    Update format-adjustment.js

--- format-adjustment.js
+++ format-adjustment.js
@@ -1,12 +1,18 @@
 window.onload = function () {
-    var formatNum = document.querySelector('a[href="tel:800.403.0688"]');
-    if (formatNum) {
-        formatNum.textContent = '1-800-403-0688';
-    } else {
-        console.log('Element not found!');
-    }
+  var formatNum = document.querySelector('a[href="tel:800.403.0688"]');
+  if (formatNum) {
+    formatNum.textContent = '1-800-403-0688';
+  } else {
+    console.log('Element not found!');
+  }
 
-    gtag('config', 'AW-1064804888/BrgACMicxl8QmMTe-wM', {
-        'phone_conversion_number': formatNum,
-    });
+  window.dataLayer = [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'G-BC4L5E2B6N');
+  gtag('config', 'AW-1064804888');
+
+  gtag('config', 'AW-1064804888/BrgACMicxl8QmMTe-wM', {
+    'phone_conversion_number': formatNum,
+  });
 }

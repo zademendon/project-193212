e47d29a8150c73cf90351be0ad3956f92d3ecdb0
zademendon <104177830+zademendon@users.noreply.github.com>
2024-11-12 12:43:22 -0300

    Update format-adjustment.js

--- format-adjustment.js
+++ format-adjustment.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+window.onload = function () {
     var formatNum = document.querySelector('a[href="tel:800.403.0688"]');
     if (formatNum) {
         formatNum.textContent = '1-800-403-0688';
@@ -7,6 +7,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     gtag('config', 'AW-1064804888/BrgACMicxl8QmMTe-wM', {
-        'phone_conversion_number': formatNum,
+        'phone_conversion_number': '1-800-123-4567',
     });
-});
+}

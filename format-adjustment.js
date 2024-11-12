a8e9b24e81b260697d93b28862b3a3af127a490e
zademendon <104177830+zademendon@users.noreply.github.com>
2024-11-12 19:38:02 -0300

    Update format-adjustment.js

--- format-adjustment.js
+++ format-adjustment.js
@@ -1,10 +1,14 @@
 window.onload = function () {
-  var formatNum = document.querySelector('a[href="tel:800.403.0688"]');
-  if (formatNum) {
-    formatNum.textContent = '1-800-403-0688';
-  } else {
-    console.log('Element not found!');
-  }
+
+  var targetNum = document.querySelector('a[href="tel:800.403.0688"]');
+  targetNum.setAttribute("id", "number")
+
+  var callback = function (formatted_number, mobile_number) {
+    var e = document.getElementById("number");
+    e.href = "tel:" + mobile_number;
+    e.innerHTML = "";
+    e.appendChild(document.createTextNode(formatted_number));
+  };
 
   window.dataLayer = [];
   function gtag() { dataLayer.push(arguments); }
@@ -14,5 +18,6 @@ window.onload = function () {
 
   gtag('config', 'AW-1064804888/BrgACMicxl8QmMTe-wM', {
     'phone_conversion_number': '1-800-403-0688',
+    'phone_conversion_callback': callback
   });
 }
